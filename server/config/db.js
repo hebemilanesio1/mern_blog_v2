990159729c1b5f71c8556dd984b530db9a817e88
Jimena Romero <romerojimena700@gmail.com>
2024-12-30 14:24:36 -0300

    Actualización con conexión al cluster de MongoDB Atlas

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://romerojimena700:45404939@cluster0.v1gd4.mongodb.net/bbdd_mern_blog";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conexión exitosa a MongoDB");
+  } catch (err) {
+    console.error("Error al conectar a MongoDB:", err);
+    process.exit(1);  // Detiene la app si falla la conexión
+  }
+};
+
+module.exports = connectDB;
